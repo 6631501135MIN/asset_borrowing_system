c4bd62886955f4866c64abcc5211e9998707d50d
Xueli03 <6531501026@lamduan.mfu.ac.th>
2025-10-30 09:36:09 +0700

    Refactor main function to use MyApp widget
    Refactored main function to use MyApp widget and removed old Scaffold structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
+// import 'User/asset_list.dart';
+import 'User/asset_menu.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App', style: TextStyle(color: Colors.grey)),
-          backgroundColor: Color.fromARGB(255, 3, 3, 139),
-        ),
-        body: Center(
-          child: Text(
-            'Hello world',
-            style: TextStyle(fontSize: 22, color: Color.fromARGB(255, 3, 3, 139)),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          backgroundColor: Color.fromARGB(255, 3, 3, 139),
-          child: Icon(Icons.add, color: Colors.grey),
-        ),
-      ),
-    ),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Borrow Request Macbook',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const BorrowRequest(),
+    );
+  }
 }
